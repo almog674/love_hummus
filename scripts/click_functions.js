37eed6b647595f920befbc17e37757e015b2a2ed
Yuval <yuvalaviv8@gmail.com>
2022-06-14 10:05:08 +0300

    Update clicks

--- scripts/click_functions.js
+++ scripts/click_functions.js
@@ -3,16 +3,12 @@ Name: click_functions.js
 Author: Hanich 02
 Purpose: Handling button presses
 */
-import HummusiotSearcher from "./hummusiot_searcher.js"
-import HummusiaRater from "./hummusiot_rater.js"
-import HummusiaAdder from "./hummusiot_adder.js"
 
-
-Searcher = HummusiotSearcher()
+Searcher = new HummusiotSearcher()
 document.getElementById("search").onclick = Searcher.get_filter_from_ui();
 
-Rater = HummusiaRater()
+Rater = new HummusiaRater()
 document.getElementById("rate").onclick = Rater.rateHummusia()
 
-Adder = HummusiaAdder()
+Adder = new HummusiaAdder()
 document.getElementById("add").onclick = Rater.addHumusia()
